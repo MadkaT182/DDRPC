e6d6fb14c90cd65b420671e96370b26c47a89d5f
unknown <madcat182@gmail.com>
2018-07-05 22:22:05 -0500

    ScreenCompany fix

--- BGAnimations/ScreenCompany decorations/default.lua
+++ BGAnimations/ScreenCompany decorations/default.lua
@@ -6,6 +6,9 @@ return Def.ActorFrame {
 	LoadActor( "konami" )..{
 		OnCommand=cmd(Center;diffusealpha,0;sleep,0.468;diffusealpha,1;sleep,4.571;linear,0.25;diffusealpha,0);
 	};
+	Def.Quad{
+		InitCommand=cmd(FullScreen;diffusecolor,color("#FFFFFF");diffusealpha,0;sleep,5.106;linear,0.467;diffusealpha,1;);
+	};
 	LoadActor( "bemani" )..{
 		OnCommand=cmd(Center;diffusealpha,0;sleep,5.106;linear,0.467;diffusealpha,1;);
 	};
