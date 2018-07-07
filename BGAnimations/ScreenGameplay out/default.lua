6fd81a3406e34f925863c0c074742e3c55ba8905
unknown <madcat182@gmail.com>
2018-07-06 22:42:04 -0500

    Updated screen timing

--- BGAnimations/ScreenGameplay out/default.lua
+++ BGAnimations/ScreenGameplay out/default.lua
@@ -6,7 +6,7 @@ return Def.ActorFrame{
 		OnCommand=cmd(x,SCREEN_WIDTH*1.5;linear,1.266;x,-SCREEN_WIDTH/2;sleep,2);
 	};
 	LoadActor("cleared")..{
-		OnCommand=cmd(Center;cropbottom,1;sleep,.866;linear,.567;cropbottom,0);
+		OnCommand=cmd(Center;cropbottom,1;sleep,.866;linear,.567;cropbottom,0;sleep,1;linear,.7;diffusealpha,0;sleep,1.667);
 	};
 	Def.Quad{
 		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-60;zoomto,0,0;sleep,.866;zoomto,460,32;diffuse,color("#000000");fadetop,.5;linear,.577;y,SCREEN_CENTER_Y+71);
